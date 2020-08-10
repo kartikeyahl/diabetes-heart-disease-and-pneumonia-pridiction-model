34740e4da515c879b5c8d0ba817d9ddc5c66eb6f
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-08-10 21:07:08 +0530

    pneumonia

---
+++ untitled0.py
@@ -0,0 +1,65 @@
+# using Convolutional Neural Network
+
+# Importing the libraries
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+tf.__version__
+
+# Data Preprocessing
+
+# Generating images for the Training set
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+# Generating images for the Test set
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+# Creating the Training set
+training_set = train_datagen.flow_from_directory('train',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+# Creating the Test set
+test_set = test_datagen.flow_from_directory('test',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+# Building the CNN
+
+# Initialising the CNN
+cnn = tf.keras.models.Sequential()
+
+#Convolution
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu", input_shape=[64, 64, 3]))
+
+ #Pooling
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+
+# Adding a second convolutional layer
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+
+# Flattening
+cnn.add(tf.keras.layers.Flatten())
+
+# Full Connection
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+#  Output Layer
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+#  Training the CNN
+
+# Compiling the CNN
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the CNN on the Training set and evaluating it on the Test set(accuracy= 90%)
+cnn.fit_generator(training_set,
+                  steps_per_epoch = 334,
+                  epochs = 25,
+                  validation_data = test_set,
+                  validation_steps = 334)
